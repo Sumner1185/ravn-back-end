193dbcce4c10ee29820fb140f92efa9a8bcf218e
Alastair Sumner <alastair-sumner@ecs-digital.co.uk>
2020-12-08 16:10:38 +0000

    Feat: add linting, prettier, coverage

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,29 +1,21 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const db = require('./queries')
-const app = express()
-const port = 3000
-
-app.use(cors())
-
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
+const express = require('express');
+const db = require('../../queries');
+const app = express();
+const port = 3000;
 
 app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API', message: "Successful connection" })
-})
-  
-app.get('/tickets', db.getTickets)
-app.get('/tickets/:id', db.getTicketsById)
-app.post('/tickets', db.createTicket)
-app.put('/tickets/:id', db.updateTicket)
-app.delete('/tickets/:id', db.deleteTicket)
+  response.json({
+    info: 'Node.js, Express, and Postgres API',
+    message: 'Welcome! Successful connection',
+  });
+});
+
+app.get('/tickets', db.getTickets);
+app.get('/tickets/:id', db.getTicketsById);
+app.post('/tickets', db.createTicket);
+app.put('/tickets/:id', db.updateTicket);
+app.delete('/tickets/:id', db.deleteTicket);
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-})
+  console.log(`App running on port ${port}.`);
+});
